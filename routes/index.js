4ba6a5e661f5c967a844696c84841f5ce40f5ac4
Stefan Idriceanu <idriceanu.stefan@gmail.com>
2021-01-27 21:00:34 +0000

    Clean useless code and add welcome message to the main path

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(200, "Welcome to the backend of the useless app");
 });
 
 module.exports = router;
--- tests/e2e/main-server.js
+++ tests/e2e/main-server.js
@@ -4,11 +4,18 @@ const expect   = require("@jest/globals").expect;
 const request  = require("supertest");
 const app      = require("../../app");
 
-describe("Server /healthcheck works", () => {
-    test("Status code is 200", async () => {
+describe("Server is running", () => {
+    test("/healthcheck endpoint returning 200 and ok", async () => {
         const healthcheckCall = await request(app).get("/healthcheck");
 
         expect(healthcheckCall.statusCode).toEqual(200);
         expect(healthcheckCall.text).toEqual("ok");
+    });
+
+    test("root endpoint returning welcome message", async () => {
+        const healthcheckCall = await request(app).get("/");
+
+        expect(healthcheckCall.statusCode).toEqual(200);
+        expect(healthcheckCall.text).toEqual("Welcome to the backend of the useless app");
     });
 });
