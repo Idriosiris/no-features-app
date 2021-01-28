a59a4e40ca4520d9a5e885d32a93be44eaf1c828
Stefan Idriceanu <idriceanu.stefan@gmail.com>
2021-01-28 23:57:58 +0000

    Add @types/supertest to make types recognisable to intellij and typescript

--- tests/e2e/main-server.ts
+++ tests/e2e/main-server.ts
@@ -6,14 +6,14 @@ describe("Server is running", () => {
     test("/healthcheck endpoint returning 200 and ok", async () => {
         const healthcheckCall = await request(app).get("/healthcheck");
 
-        expect(healthcheckCall.statusCode).toEqual(200);
+        expect(healthcheckCall.status).toEqual(200);
         expect(healthcheckCall.text).toEqual("ok");
     });
 
     test("root endpoint returning welcome message", async () => {
         const healthcheckCall = await request(app).get("/");
 
-        expect(healthcheckCall.statusCode).toEqual(200);
+        expect(healthcheckCall.status).toEqual(200);
         expect(healthcheckCall.text).toEqual("Welcome to the backend of the useless app");
     });
 });
