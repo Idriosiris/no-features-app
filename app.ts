682f7d86176ea861e5be181250f90c66177de4c8
Stefan Idriceanu <idriceanu.stefan@gmail.com>
2021-01-31 15:22:22 +0000

    Move all initialisation methods to app class

--- app.ts
+++ app.ts
@@ -1,24 +1,5 @@
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-
-import indexRouter from './routes';
 import {App} from "./app1";
 
 let app = new App().theApp();
-
-initMiddlewares();
-initRoutes();
-
-function initMiddlewares() {
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({extended: false}));
-    app.use(cookieParser());
-}
-
-function initRoutes() {
-    app.use('/', indexRouter);
-}
 
 export default app;
--- app1.ts
+++ app1.ts
@@ -1,10 +1,27 @@
 import express, {Express} from "express";
+import logger from "morgan";
+import cookieParser from "cookie-parser";
+import indexRouter from "./routes";
 
 export class App {
     private app: Express;
 
     constructor() {
         this.app = express();
+
+        this.initMiddlewares();
+        this.initRoutes();
+    }
+
+    initMiddlewares() {
+        this.app.use(logger('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(cookieParser());
+    }
+
+    initRoutes() {
+        this.app.use('/', indexRouter);
     }
 
     theApp(){
