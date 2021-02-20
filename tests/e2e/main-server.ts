8b994bdba8e41f0a9cfee81fface1b2d341ff693
Stefan Idriceanu <idriceanu.stefan@gmail.com>
2021-02-20 20:11:15 +0000

    Have test dependencies as fields

--- tests/e2e/main-server.ts
+++ tests/e2e/main-server.ts
@@ -2,24 +2,24 @@ import {describe, test, expect} from '@jest/globals';
 import request from 'supertest';
 import {App} from "../../src/app";
 import logger from "morgan";
-import express from "express";
+import express, {Express} from "express";
 import cookieParser from "cookie-parser";
 import indexRouter from "../../src/routes";
 import {ErrorEventHandler} from "../../src/config/error-event-handler";
 
+let app: Express = new App([
+    logger('dev'),
+    express.json(),
+    express.urlencoded({extended: false}),
+    cookieParser()
+], [
+    indexRouter
+], [
+    new ErrorEventHandler()
+]).theApp();
+
 describe("Server is running", () => {
     test("/healthcheck endpoint returning 200 and ok", async () => {
-        let app = new App([
-            logger('dev'),
-            express.json(),
-            express.urlencoded({extended: false}),
-            cookieParser()
-        ], [
-            indexRouter
-        ], [
-            new ErrorEventHandler()
-        ]).theApp()
-
         const healthcheckCall = await request(app).get("/healthcheck");
 
         expect(healthcheckCall.status).toEqual(200);
@@ -27,17 +27,6 @@ describe("Server is running", () => {
     });
 
     test("root endpoint returning welcome message", async () => {
-        let app = new App([
-            logger('dev'),
-            express.json(),
-            express.urlencoded({extended: false}),
-            cookieParser()
-        ], [
-            indexRouter
-        ], [
-            new ErrorEventHandler()
-        ]).theApp()
-
         const healthcheckCall = await request(app).get("/");
 
         expect(healthcheckCall.status).toEqual(200);
--- tests/integration/database-connection-test.ts
+++ tests/integration/database-connection-test.ts
@@ -2,12 +2,10 @@ import {describe, expect, test} from '@jest/globals';
 import {DatabaseHealthcheckRepository} from "../../src/repositories/database-healthcheck-repository";
 import DatabaseConnection from "../../src/config/database-connection";
 
+let databaseHealthcheckRepository = new DatabaseHealthcheckRepository(DatabaseConnection.connection());
+
 describe("Database is running", () => {
     test("can perform SELECT query", async () => {
-        let databaseHealthcheckRepository;
-
-        databaseHealthcheckRepository = new DatabaseHealthcheckRepository(DatabaseConnection.connection());
-
         let healthcheck = await databaseHealthcheckRepository.databaseHealthCheck();
 
         expect(healthcheck).toEqual("Database up and running!");
